a9af798e2ffafb09f78d8d421d73908403f3a65c
Jameson Brown <jamesonbrown@neolivity.com>
2024-06-06 21:57:33 -0600

    Updated list of brews

--- src/Brewlist.jsx
+++ src/Brewlist.jsx
@@ -52,6 +52,14 @@ export const BrewList = () => {
   )
 }
 
+const STATUSES = {
+  CONCEPT: "CONCEPT",
+  PLANNING: "PLANNING",
+  BREWING: "BREWING",
+  KEGGED: "KEGGED",
+  SOLDOUT: "SOLD OUT",
+}
+
 const brews = [
   {
     id: 1,
@@ -61,7 +69,7 @@ const brews = [
     colors: ["from-[#eb8bb9]", "to-[#ed975a]"],
     brewed_by: "Atomic10 Studios + Barely Fiction Exp",
     available_count: "10",
-    status: "KEGGED",
+    status: STATUSES.KEGGED,
     description:
       "Indulge in the delightful essence of summer with our Peach Cobbler Seltzer. This beverage captures the sweet, juicy flavor of ripe peaches perfectly balanced with a hint of creamy vanilla, reminiscent of the classic dessert.",
     event_details:
@@ -70,38 +78,40 @@ const brews = [
   {
     id: 2,
     name: "Passionfruit Cider",
-    estimated_release: new Date("June 14, 2024"),
+    estimated_release: new Date("June 22, 2024"),
     estimated_abv: "6%",
     colors: ["from-[#b000b5]", "to-[#bada55]"],
     brewed_by: "Atomic10 Studios + Barely Fiction Exp",
     available_count: "10",
-    status: "PLANNING",
+    status: STATUSES.BREWING,
     description:
       "Experience the tropical allure of our Passionfruit Cider. A vibrant and tantalizing beverage that brings an exotic twist to the traditional cider. Bursting with the tangy, aromatic flavor of passionfruit, this cider offers a refreshing bite balanced by the natural sweetness of crisp apples.",
     event_details:
-      "Come on a tropical getaway and try our Passionfruit Cider. It's just as good as it sounds and goes great with our island reggae playlist. Bring your sun kissed date and fantasize about the perfect exotic vacation.",
+      "Come on a tropical getaway and try our Passionfruit Cider. It's just as good as it sounds and goes great with our island reggae playlist. Bring your sun kissed date, coconut scented sunscreen, and fantasize about the perfect exotic vacation.",
   },
   {
     id: 3,
     name: "Ghastly Grape Soda",
-    estimated_release: new Date("June 20, 2024"),
+    estimated_release: new Date("June 29, 2024"),
     estimated_abv: "5.2%",
     colors: ["from-[#ecdbf8]", "to-[#8359a3]"],
-    brewed_by: "Atomic10 Studios",
+    brewed_by: "Atomic10 Studios + Barely Fiction Exp",
     available_count: "10",
-    status: "PLANNING",
+    status: STATUSES.PLANNING,
     description:
       "The frighteningly deep purple color of this hard soda with all the lusciousness of the concord grape is exactly what you are expecting. Its tangy, sweet, and undeniably grapalicious.",
+    event_details:
+      "Take a trip back in time with our Ghastly Grape Soda. This is the first in our hard soda collection. Promises a unique twist on your favorite childhood soda, now with an adult-friendly twist. Paired with games and snacks to take you back to an afternoon after school.",
   },
   {
     id: 4,
     name: "Dank NEIPA",
-    estimated_release: new Date("June 27, 2024"),
-    estimated_abv: "8.5%",
+    estimated_release: new Date("July 4, 2024"),
+    estimated_abv: "6.5%",
     colors: ["from-[#dec744]", "to-[#a2dbec]"],
     brewed_by: "Atomic10 Studios",
     available_count: "10",
-    status: "PLANNING",
+    status: STATUSES.PLANNING,
     description:
       "Hazy & chock full of juicy fruit flavors, our Dank NEIPA is silky-smooth with a creamy mouthfeel. Packed with Mosaic and Cascade hops this IPA is light on bitterness, super-hazy in color and heavy on delicious hop flavor.",
   },
@@ -109,13 +119,15 @@ const brews = [
     id: 5,
     name: "Liger's Blood Cider",
     estimated_release: new Date("July 4, 2024"),
-    estimated_abv: "6.1%",
+    estimated_abv: "6%",
     colors: ["from-[#ff5946]", "to-[#ffffff]"],
     brewed_by: "Atomic10 Studios",
     available_count: "10",
-    status: "CONCEPT",
+    status: STATUSES.CONCEPT,
     description:
       "A strawberry and coconut teamed up to create this Liger's Blood cider. An adult snow cone, minus the snow and the cone, but with all the flavor. Fresh whole strawberries with a hint of coconut adds a refreshing tropical twist.",
   },
-  // Fruit punch cider
+  // Hawaiian punch cider/seltzer
+  // Orange soda
+  // Ruby red grapefruit soda
 ]
