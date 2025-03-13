5c120a9cdf52c1c58d27ba19a98ae1b38b4617f3
Jameson Brown <jamesonbrown@neolivity.com>
2025-03-12 22:50:04 -0600

    Updated brewlist

--- src/Brewlist.jsx
+++ src/Brewlist.jsx
@@ -28,7 +28,8 @@ export const BrewList = () => {
           })
           .map((brew) => {
             const [start = "", finish = ""] = brew.colors
-            const rowStyles = `opacity-90 bg-gradient-to-r ${start} ${finish} rounded-md p-12 mb-16 cursor-pointer relative`
+            const rowStyles = `opacity-90 bg-gradient-to-r ${start} ${finish} rounded-md p-12 mb-16 cursor-pointer relative group hover:opacity-100 transition-all duration-300`
+            const contentStyles = `transition-all duration-300 group-hover:scale-110 group-hover:opacity-100`
 
             return (
               <div
@@ -36,7 +37,11 @@ export const BrewList = () => {
                 key={brew.id}
                 onClick={() => setCurrentBrew(brew)}
               >
-                <p className="text-medium dark:text-[#213547]">{brew.name}</p>
+                <p
+                  className={`text-medium dark:text-[#213547] ${contentStyles}`}
+                >
+                  {brew.name}
+                </p>
 
                 <p className="text-small mb-[4px] dark:text-[#213547]">
                   Status: {brew.status}
@@ -56,30 +61,27 @@ export const BrewList = () => {
                   ].includes(brew.status) && "Est "}
                   ABV: {brew.estimated_abv}
                 </p>
-                {/* <hr className="mx-16 my-6" /> */}
                 <p className="text-small rounded-md bg-opacity-50 bg-[seashell] p-6 dark:text-[#213547]">
                   {brew.description}
                 </p>
                 <p className="text-small mt-6 dark:text-[#213547]">
                   Brewed by: {brew.brewed_by}
                 </p>
-                {[STATUSES.GONE, STATUSES.KEGGED].includes(brew.status) && (
-                  <>
-                    {brew.status === STATUSES.GONE ? (
-                      <p className="absolute top-[24px] right-[6px] rotate-[40deg] text-small dark:text-[#213547] [text-shadow:_1.5px_1.5px_0_rgb(256_256_256_/_50%)]">
-                        ALL GONE
-                      </p>
-                    ) : (
-                      <p className="absolute top-[28px] right-0 rotate-[35deg] text-small dark:text-[#213547] [text-shadow:_1.5px_1.5px_0_rgb(256_256_256_/_50%)]">
-                        GRAB A GLASS
-                      </p>
-                    )}
-                    {brew.status === STATUSES.GONE && (
-                      <p className="absolute top-[24px] left-[6px] -rotate-[40deg] text-small dark:text-[#213547] [text-shadow:_1.5px_1.5px_0_rgb(256_256_256_/_50%)]">
-                        ALL GONE
-                      </p>
-                    )}
-                  </>
+                {/* Status tags */}
+                {brew.status === STATUSES.GONE && (
+                  <p className="absolute top-[24px] right-[6px] rotate-[40deg] text-small dark:text-[#213547] [text-shadow:_1.5px_1.5px_0_rgb(256_256_256_/_50%)]">
+                    ALL GONE
+                  </p>
+                )}
+                {brew.status === STATUSES.KEGGED && (
+                  <p className="absolute top-[28px] right-0 rotate-[35deg] text-small dark:text-[#213547] [text-shadow:_1.5px_1.5px_0_rgb(256_256_256_/_50%)]">
+                    GRAB A GLASS
+                  </p>
+                )}
+                {brew.status === STATUSES.GONE && (
+                  <p className="absolute top-[24px] left-[6px] -rotate-[40deg] text-small dark:text-[#213547] [text-shadow:_1.5px_1.5px_0_rgb(256_256_256_/_50%)]">
+                    ALL GONE
+                  </p>
                 )}
               </div>
             )
@@ -117,10 +119,10 @@ const brews = [
     name: "Ghastly Grape Soda",
     estimated_release: new Date("June 29, 2024"),
     estimated_abv: "5.2%",
-    colors: ["from-[#ecdbf8]", "to-[#8359a3]"],
+    colors: ["from-[#8359a3]", "to-[#ecdbf8]"],
     brewed_by: "Atomic10 Studios + Barely Fiction Exp",
     available_count: "10",
-    status: STATUSES.KEGGED,
+    status: STATUSES.GONE,
     description:
       "The frighteningly deep purple color of this hard soda with all the lusciousness of the concord grape is exactly what you are expecting. Its tangy, sweet, and undeniably grapalicious.",
     event_details:
@@ -143,12 +145,12 @@ const brews = [
   {
     id: 4,
     name: "Passionfruit Cider #2",
-    estimated_release: new Date("September 28, 2024"),
-    estimated_abv: "6.56%",
+    estimated_release: new Date("October 11, 2024"),
+    estimated_abv: "6.5%",
     colors: ["from-[#b000b5]", "to-[#bada55]"],
     brewed_by: "Atomic10 Studios + Barely Fiction Exp",
     available_count: "10",
-    status: STATUSES.BREWING,
+    status: STATUSES.GONE,
     description:
       "Back by popular demand! Experience the tropical allure of our Passionfruit Cider. A vibrant and tantalizing beverage that brings an exotic twist to the traditional cider. Bursting with the tangy, aromatic flavor of passionfruit, this cider offers a refreshing bite balanced by the natural sweetness of crisp apples.",
     event_details:
@@ -157,12 +159,12 @@ const brews = [
   {
     id: 5,
     name: "Carmel Apple Cider",
-    estimated_release: new Date("Septempber 28, 2024"),
-    estimated_abv: "6.5%",
+    estimated_release: new Date("October 11, 2024"),
+    estimated_abv: "6.4%",
     colors: ["from-[#8B0000]", "to-[#90EE90]"],
     brewed_by: "Atomic10 Studios + Barely Fiction Exp",
     available_count: "10",
-    status: STATUSES.BREWING,
+    status: STATUSES.GONE,
     description:
       "Savor the essence of autumn with our Caramel Apple Cider. This delightful brew marries the crisp, refreshing taste of fresh apples with the rich, indulgent sweetness of caramel. Each sip offers a perfect balance of fruity and buttery notes, reminiscent of a classic fall treat. Experience the cozy warmth of the season in every glass of this nostalgic, expertly crafted cider.",
     event_details: "More details coming soon.",
@@ -170,19 +172,19 @@ const brews = [
   {
     id: 6,
     name: "Bootlegger's Root Beer",
-    estimated_release: new Date("September 28, 2024"),
-    estimated_abv: "6.5%",
+    estimated_release: new Date("April 12, 2025"),
+    estimated_abv: "5.5%",
     colors: ["from-[#B87333]", "to-[#CD5C5C]"],
-    brewed_by: "Atomic10 Studios",
-    available_count: "10",
-    status: STATUSES.BREWING,
+    brewed_by: "Atomic10 Studios + Barely Fiction Exp",
+    available_count: "20",
+    status: STATUSES.PLANNING,
     description:
       "Indulge in our Bootlegger's Root Beer, a nostalgic treat with a grown-up twist. This creamy, smooth, and deliciously rooty brew combines the classic flavors of root beer with a surprising alcoholic kick. Crafted with real root beer extract and a hint of vanilla, it offers a rich, satisfying taste that harkens back to simpler times. Savor the complex blend of traditional root beer essence in every sip of this innovative, craft beverage.",
   },
   {
     id: 7,
     name: "Dank NEIPA",
-    estimated_release: new Date("October 4, 2024"),
+    estimated_release: new Date("April 13, 2025"),
     estimated_abv: "6.5%",
     colors: ["from-[#dec744]", "to-[#a2dbec]"],
     brewed_by: "Atomic10 Studios",
@@ -193,9 +195,21 @@ const brews = [
   },
   {
     id: 8,
+    name: "Spiked Sunset Soda",
+    estimated_release: new Date("May 16, 2025"),
+    estimated_abv: "5.5%",
+    colors: ["from-[#FFAA33]", "to-[#35B56E]"],
+    brewed_by: "Atomic10 Studios + Barely Fiction Exp",
+    available_count: "10",
+    status: STATUSES.CONCEPT,
+    description:
+      "A refreshing blend of orange and pineapple, this hard soda is a perfect summer drink. It's light and bubbly, with a hint of sweetness from the orange and a tangy kick from the pineapple. You will want to sip this all summer long.",
+  },
+  {
+    id: 9,
     name: "Liger's Blood Cider",
-    estimated_release: new Date("October 4, 2024"),
-    estimated_abv: "6%",
+    estimated_release: new Date("May 16, 2025"),
+    estimated_abv: "6.5%",
     colors: ["from-[#ff5946]", "to-[#ffffff]"],
     brewed_by: "Atomic10 Studios",
     available_count: "10",
@@ -204,10 +218,10 @@ const brews = [
       "A strawberry and coconut teamed up to create this Liger's Blood cider. An adult snow cone, minus the snow and the cone, but with all the flavor. Fresh whole strawberries with a hint of coconut adds a refreshing tropical twist.",
   },
   {
-    id: 9,
+    id: 10,
     name: "Cranberry Mint Seltzer",
-    estimated_release: new Date("November 24, 2024"),
-    estimated_abv: "6.5%",
+    estimated_release: new Date("May 30, 2025"),
+    estimated_abv: "5.5%",
     colors: ["from-[#DC143C]", "to-[#98FB98]"],
     brewed_by: "Atomic10 Studios",
     available_count: "10",
